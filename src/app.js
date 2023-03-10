ea3f0a40e6d24d7034482b188dd7e600888a7277
p.istvan <pasztoristvan62@gmail.com>
2023-03-10 10:46:29 +0100

    A dolgozok tömbje

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const tbody =document.querySelector('#tbody')
+
+
+var dolgozLista=[    
+    { name:'Pisti', city: 'szolnok', salary: 355},
+    { name:'Dani', city: 'budapest', salary: 355},
+    { name:'Kati', city: 'pest', salary: 355},
+    { name:'Béla', city: 'sehol', salary: 355},
+    
+];
+
+console.log (tbody)
